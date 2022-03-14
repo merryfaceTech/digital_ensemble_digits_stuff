01f50d66bddd423908a0947cfc2153abcfac42c0
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-03-14 17:56:15 +0000

    this is silly

--- digital_root.rb
+++ digital_root.rb
@@ -1,27 +1,7 @@
 class Digital_root
   def execute(n)
-    integer_array = []
-    
-    while integer_array.sum > 10
-      digits_array = n.to_s.split('')
-      digits_array.each do |digit|
-        integer_array << digit.to_i
-      end 
-    end
-
-    if integer_array.sum > 10
-      second_integer_array = []
-
-      integer_array.sum.to_s.split('').each do |digit|
-        second_integer_array << digit.to_i
-      end
-
-      return second_integer_array.sum
-    end
-
-    return integer_array.sum
+    n < 10 ? n : execute(n.digits.sum)
   end
 end
 
-
-
+# WHY YOU DIGIT BACKWARDS?!
